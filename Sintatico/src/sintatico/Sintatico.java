2e13abc3b99ed88ffb86dafe658c58a7500ea301
Marcos Avila Isidoro <marcavis@yahoo.com.br>
2018-09-16 00:11:34 -0300

    remover teste antigo

--- Sintatico/src/sintatico/Sintatico.java
+++ Sintatico/src/sintatico/Sintatico.java
@@ -240,45 +240,5 @@ public class Sintatico {
 		// TODO Auto-generated method stub
 		return "prog begin read ( foo ) ; end.";
 	}
-
-	public static void exercicio1G1() {
-		Gramatica gG = new Gramatica("A");
-		HashMap<String, Boolean> tiposDeSimbolo = gG.getTiposDeSimbolo();
-		tiposDeSimbolo.put("A", false); //A é um token não-terminal
-		tiposDeSimbolo.put("q",true);
-		tiposDeSimbolo.put("y",true);
-		tiposDeSimbolo.put("k",true);
-		tiposDeSimbolo.put("x",true);
-		tiposDeSimbolo.put("m",true);
-		tiposDeSimbolo.put("r",true);
-		tiposDeSimbolo.put("W",false);
-		tiposDeSimbolo.put("1",true);
-		
-		//gG.addProducao(new Producao("A", new ArrayList<String>(Arrays.asList("A", "y"))));
-		gG.addProducao(new Producao("A", new ArrayList<String>(Arrays.asList("k"))));
-		//gG.addProducao(new Producao("A", new ArrayList<String>(Arrays.asList("A", "x"))));
-		gG.addProducao(new Producao("A", new ArrayList<String>(Arrays.asList("m", "W"))));
-		
-		//esses dois nao existem no exemplo
-		//gG.addProducao(new Producao("A", new ArrayList<String>(Arrays.asList("W","y"))));
-		//gG.addProducao(new Producao("W", new ArrayList<String>(Arrays.asList("A","y"))));
-		
-		gG.addProducao(new Producao("W", new ArrayList<String>(Arrays.asList("r"))));
-		gG.addProducao(new Producao("W", new ArrayList<String>(Arrays.asList("k"))));
-		gG.addProducao(new Producao("W", new ArrayList<String>(Arrays.asList("1"))));
-		gG.addProducao(new Producao("W", new ArrayList<String>(Arrays.asList("y"))));
-		//ArrayList<String> places = new ArrayList<String>(Arrays.asList("Buenos Aires", "Córdoba", "La Plata"));
-		HashMap<String, ArrayList<String>> first = gG.first();
-		
-		System.out.println("first:");
-		System.out.println(first);
-		
-		System.out.println(gG.primeiraRegra(true));
-		
-		gG.listaRegras();
-		
-	}
 	
-	
-
 }
